602ff2a103ec39856aa0d78773c652c86e5c422c
冯守博 <shbfeng@msn.com>
2019-04-24 14:00:52 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChnNumProc
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string box_string = InBox.Text;
+
+            Int32 input_data = System.Convert.ToInt32(box_string); 
+            NumToChn UnumTochn = new NumToChn();
+            string result = UnumTochn.NumberToChinese(input_data);
+            OutBox.Text = result;
+        }
+    }
+}
---
+++ NumToChn.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChnNumProc
+{
+    class NumToChn
+    {
+        private readonly string[] _chnNumChar =  new string []{"零","一", "二", "三", "四", "五", "六", "七", "八", "九"};
+        private readonly string[] _chnUnitSection = new string []{"", "万", "亿", "万亿"};
+        private readonly string[] _chnUnitChar = new string []{"", "十", "百", "千"};
+        public string NumberToChinese(Int32 num)
+        {
+            int unitPos = 0;
+            string strIns = "";
+            bool needZero = false;
+            string chnStr = "";
+            while (num > 0) 
+            {
+                Int32 section = num % 100000;
+                if (needZero)
+                {
+                    chnStr =chnStr.Insert(0, _chnNumChar[0]);
+                }
+                chnStr = SectionToChinese(section);
+                //是否需要节权位
+                strIns += (section != 0) ? _chnUnitSection[unitPos] : _chnUnitSection[0];
+                chnStr = chnStr.Insert(0, strIns);
+                needZero = (section < 1000) && (section > 0);
+                num = num / 10000;
+                unitPos++;
+            }
+
+            return chnStr;
+        }
+
+        public string SectionToChinese(Int32 section)
+        {
+            string strIns;
+            int unitPos = 0;
+            bool zero = true;
+            string chnStr = "";
+            while (section > 0)
+            {
+                int v = section % 10;
+                if (v == 0)
+                {
+                    if ((section == 0) || !zero)
+                    {
+                        zero = true;
+                        chnStr= chnStr.Insert(0, _chnNumChar[v]);
+
+                    }
+                }
+                else
+                {
+                    zero = false;
+                    strIns = _chnNumChar[v];
+                    strIns += _chnUnitChar[unitPos];
+                    chnStr = chnStr.Insert(0, strIns);
+                }
+
+                unitPos++;
+                section = section / 10;
+            }
+
+            return chnStr;
+        }
+    }
+}
